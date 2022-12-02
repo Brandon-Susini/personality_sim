59ae3c527cb8747f900f70b6ffc270ff25ce045d
motazmo <brandonsusini@gmail.com>
2022-12-02 08:41:24 -0500

    Converted Elements to Enums. Made new Type struct to hold them.

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,31 @@ use bevy::prelude::*;
 //E,I -> Attitudes: 
 //  Extraverted (Objective) & Introverted (Expected)
 */
-#[derive(Debug, Component)]
-struct N;
+//#[derive(Debug, Component)]
+/* struct N;
 #[derive(Debug, Component)]
 struct S;
 #[derive(Debug, Component)]
 struct T;
 #[derive(Debug, Component)]
-struct F;
+struct F; */
+#[derive(Debug)]
+enum Element{
+    N,
+    S,
+    F,
+    T,
+}
+#[derive(Debug,Component)]
+struct Type{
+    elements: Vec<Element>
+}
+//Type{elements: vec![Element::N]}
+impl Type{
+    fn new(element: Element) -> Type{
+        Type{elements: vec!(element)}
+    }
+}
 
 // ^ Convert these to enums and use those for the type
 /* struct Type{
@@ -37,40 +54,69 @@ struct Name(String);
 #[derive(Debug,Resource)]
 struct SimTimer(Timer);
 //Interface for elements.
-trait Element{
+/* trait Element{
     fn is_observer(&self) -> bool;
 }
 impl Element for N{
     fn is_observer(&self) -> bool{
         true
     }
+} */
+/* #[derive(Debug,Component)]
+struct TimeKeeper{
+    ticks_passed: i32
 }
-
+impl TimeKeeper{
+    fn new()->TimeKeeper{
+        TimeKeeper{ticks_passed: 0}
+    }
+    fn advance(&mut self){
+        self.ticks_passed += 1;
+    }
+} */
+#[derive(Debug,Component)]
+struct TimeKeeper(i32);
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(SimTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+        .add_startup_system(start)
         .add_startup_system(add_people)
-        .add_startup_system(print_people)
+        .add_system(print_people)
         .add_system(sim_tick)
         .run();
 }
-fn sim_tick(time: Res<Time>, mut timer: ResMut<SimTimer>){
+fn start(mut commands: Commands){
+    commands.spawn(TimeKeeper(0));
+}
+/*
+    Potentially turn this if statement into a macro that takes an argument for the string to be printed.
+    Look at the get_component_mut example and figure that out
+    */
+fn sim_tick(time: Res<Time>, mut timer: ResMut<SimTimer>, mut tk: Query<&mut TimeKeeper>){
     if timer.0.tick(time.delta()).just_finished(){
-        println!("Tick");
+        //Every tick...
+        for mut t in tk.iter_mut(){
+            t.0 += 1;
+            println!("Tick {}",t.0);
+            //tk.get_component_mut(entity)
+        }
     }
 }
 //Move the mut to the type definition and see what happens.
-
 fn add_people(mut commands: Commands){
-    commands.spawn((Person,Name("Ne".to_string()), N));
+    /* commands.spawn((Person,Name("Ne".to_string()), N));
     commands.spawn((Person, Name("Ni".to_string()), N));
-    commands.spawn((Person, Name("Si".to_string()), S));
+    commands.spawn((Person, Name("Si".to_string()), S)); */
+    commands.spawn((Person,Name("Ne".to_string()), Type::new(Element::N)));
+    //commands.spawn((Person, Name("Ni".to_string()), Element::N));
+    //commands.spawn((Person, Name("Si".to_string()), Element::T));
 }
 
 
-fn print_people(search: Query<&Name, (With<Person>,With<S>)>){
-    for name in search.iter(){
-        println!("{}",name.0);
+fn print_people(search: Query<&Type, With<Person>>){
+    for t in search.iter(){
+        ()
+        //println!("{:?}",t.elements[0]);
     }
 }
